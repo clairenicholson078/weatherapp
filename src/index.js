4059c7d08b89ce2f05fb5ba73c4319420bdcc889
clairenicholson078 <80160750+clairenicholson078@users.noreply.github.com>
2021-07-21 13:27:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -71,5 +71,3 @@ form.addEventListener("submit", submitCity);
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
   axios.get(apiUrl).then(alertWeather);
 }
-               
-
