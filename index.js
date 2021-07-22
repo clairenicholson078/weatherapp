ab6ec45c1b0df58bd4fe5f2ffdcf4ee89241004c
clairenicholson078 <80160750+clairenicholson078@users.noreply.github.com>
2021-07-22 08:21:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,18 @@ function formatDate(timestamp) {
   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
-}function displayForecast(response) {
+}
+
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
